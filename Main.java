d846b11197213dc4238979f56c2c51cc555cbb82
MariiaFlora <pivovarenkomariia@gmai.com>
2024-09-30 14:08:10 +0300

    Added celsius to fahrenheit conversion functionality

--- Main.java
+++ Main.java
@@ -6,10 +6,13 @@ public class Main {
         return (fahrenheit - 32) * 5 / 9;
     }
 
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
     public static void main(String[] args) {
         System.out.println("Converter App.");
         System.out.println("App for converting temperature measures.");
         System.out.println("Version 1.0.");
     }
-
 }
