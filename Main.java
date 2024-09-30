88bd1babe190d0d83c9e5cee5fb18d1f09c598fe
MariiaFlora <pivovarenkomariia@gmai.com>
2024-09-30 13:52:56 +0300

    Added fahrenheit to celsius conversion functionality

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 package app;
 
 public class Main {
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
     public static void main(String[] args) {
         System.out.println("Converter App.");
         System.out.println("App for converting temperature measures.");
         System.out.println("Version 1.0.");
     }
+
 }
